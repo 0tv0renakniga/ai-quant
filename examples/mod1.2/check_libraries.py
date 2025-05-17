2445b957885e1a13f32db789aecc959016311a5d
0tv0renakniga <rroge036@gmail.com>
2025-05-16 22:00:02 -0600

    add mod 1.2 material

---
+++ examples/mod1.2/check_libraries.py
@@ -0,0 +1,11 @@
+# Import essential libraries to confirm successful installation
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn
+import yfinance as yf
+import statsmodels.api as sm
+
+print("All libraries imported successfully!")
+
